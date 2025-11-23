4d11accabdf19a1c0141684dfb7498788fec1ebc
s3til <jimmyhankers@hotmail.com>
2025-11-23 03:46:34 -0500

    Add files via upload

---
+++ PCont.cs
@@ -0,0 +1,51 @@
+using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using Range = SemanticVersioning.Range;
+
+
+namespace _PCont;
+
+// This record holds the various properties for your mod
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.s3til.PCont";
+    public override string Name { get; init; } = "PCont";
+    public override string Author { get; init; } = "s3til";
+    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+    public override Range SptVersion { get; init; } = new("4.0.6");
+    public override string License { get; init; } = "MIT";
+    public override bool? IsBundleMod { get; init; } = true;
+    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    {
+        { "com.wtt.commonlib", new Range("~2.0.0") }
+    };
+    public override string? Url { get; init; }
+    public override List<string>? Contributors { get; init; }
+    public override List<string>? Incompatibilities { get; init; }
+}
+
+/// <summary>
+/// Feel free to use this as a base for your mod
+/// </summary>
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 2)]
+public class PCOn(
+    WTTServerCommonLib.WTTServerCommonLib wttCommon
+) : IOnLoad
+{
+    public async Task OnLoad()
+    {
+
+        // Get your current assembly
+        var assembly = Assembly.GetExecutingAssembly();
+
+
+
+
+        // Use WTT-CommonLib services
+        await wttCommon.CustomHideoutRecipeService.CreateHideoutRecipes(assembly);
+
+        await Task.CompletedTask;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# progressivestashupdate
-leaves progressive stash redone for 4.0
+
+# PCont v1
+
+### Dependancies
+    WTTCommonLib
+
+leaves progressive stash remade for 4.0 using WTT tools. big thanks to the WTT people for making that public
+
+also a thank you to Colobos9mm for redoing Virtuals Custom Quests using WTT and being one of the only ""normal"" wtt mods i could find so i could teach myself to use WTT
+
+anyways other than that read the page on the sptaki site for an actual description of the mod
+ 
